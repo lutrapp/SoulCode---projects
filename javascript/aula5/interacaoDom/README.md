ac5f705a4e1b7cf3b5925da6ff9f98d993d97b50
lutrapp <lutrapp@gmail.com>
2021-04-16 17:34:18 -0300

    aula 4 iniciando elementos dom

---
+++ javascript/aula5/interacaoDom/README.md
@@ -0,0 +1,11 @@
+# SoulCode---projects
+
+# javascript
+
+1) Crie um site personalizado por você que tenha um botão em html que ao ser clicado acesse uma função que peça para o usuário digitar um numero N que seja maior que 0 e menor que 21. Você deverá armazenar em um Array de números uma sequência de N (Que foi o número digitado pelo usuário) números pares, a partir do número 2. 
+
+Exs: Usuário digitou 5
+Deverá criar um Array contendo os números 2, 4, 6, 8 e 10.
+
+Usuário digitou 3
+Deverá criar um Array contendo os números 2 4 e 6.
---
+++ javascript/aula5/interacaoDom/script1.js
@@ -0,0 +1,17 @@
+var lista = [];
+
+var btn = document.querySelector('button');
+btn.onclick = function(){
+    var numero = parseInt(prompt("Digite um número entre 0 e 21"));
+    if(numero > 0 && numero <21){
+        for(x=1; x<=numero; x++){
+            var indice = 2*x; 
+            lista[x-1] = indice;
+        }
+        console.log(lista);
+        document.getElementById('divText').innerHTML="Preciso fazer: " +lista+ "...vezes. Nunca desista!";
+    }else{
+        alert("O número que você digitou não estava no intervalo solicitado! Tente novamente!")
+    }
+}
+
